948f3ff8219839dce49965bbfbd15a6f758175f8
Dmitry Dorofeev <djudman@gmail.com>
2016-04-24 12:33:32 +0300

    getting evernote access token

--- evernoterobot/libevernote/client.py
+++ evernoterobot/libevernote/client.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from libevernote import EvernoteSdk
 
 
@@ -8,13 +9,15 @@ class Evernote:
                                consumer_secret=consumer_secret,
                                sandbox=True)
         self.oauth_url = None
+        self.oauth_token = None
 
     def get_oauth_url(self, callback_url):
         if not self.oauth_url:
             request_token = self.sdk.get_request_token(callback_url)
+            self.oauth_token = urllib.parse.quote(request_token['oauth_token'])
             self.oauth_url = self.sdk.get_authorize_url(request_token)
         return self.oauth_url
 
-    def get_access_token(self, oauth_token, oauth_verifier):
-        return self.sdk.get_access_token(oauth_token,
-                                         oauth_verifier=oauth_verifier)
+    def get_access_token(self, oauth_token_secret, oauth_verifier):
+        return self.sdk.get_access_token(self.oauth_token, oauth_token_secret,
+                                         oauth_verifier)
