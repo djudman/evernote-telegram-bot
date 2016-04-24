4421291f77162d0293d93b6be051e7810da38e87
Dmitry Dorofeev <djudman@gmail.com>
2016-04-24 22:42:29 +0300

    fix

--- evernoterobot/telegram/robot.py
+++ evernoterobot/telegram/robot.py
@@ -78,25 +78,25 @@ Please tap on button below to link your Evernote account with me.'''
         inline_keyboard = [[signin_button]]
         message = await self.api.sendMessage(self.chat_id, welcome_text,
                                              inline_keyboard)
-        startsession = await session.get_start_session(self.user.id)
-        if not startsession:
-            callback_key = self.get_callback_key(self.user.id)
-            callback_url = "%(callback_url)s?key=%(key)s" % {
-                    'callback_url': self.evernote_oauth_callback,
-                    'key': callback_key,
-                }
-            request_token = self.evernote.get_request_token(callback_url)
-            oauth_token = request_token['oauth_token']
-            oauth_token_secret = request_token['oauth_token_secret']
-            # TODO: put tokens to cache
-            oauth_url = self.evernote.get_authorize_url(request_token)
-            await session.save_start_session(self.user.id, oauth_url,
-                                             callback_key)
-        else:
-            oauth_url = startsession['oauth_url']
+        # startsession = await session.get_start_session(self.user.id)
+        # if not startsession:
+        #     callback_key = self.get_callback_key(self.user.id)
+        #     callback_url = "%(callback_url)s?key=%(key)s" % {
+        #             'callback_url': self.evernote_oauth_callback,
+        #             'key': callback_key,
+        #         }
+        #     request_token = self.evernote.get_request_token(callback_url)
+        #     oauth_token = request_token['oauth_token']
+        #     oauth_token_secret = request_token['oauth_token_secret']
+        #     # TODO: put tokens to cache
+        #     oauth_url = self.evernote.get_authorize_url(request_token)
+        #     await session.save_start_session(self.user.id, oauth_url,
+        #                                      callback_key)
+        # else:
+        #     oauth_url = startsession['oauth_url']
 
         signin_button['text'] = 'Sign in to Evernote'
-        signin_button['url'] = oauth_url
+        signin_button['url'] = 'oauth_url'
         await self.api.editMessageReplyMarkup(
             self.chat_id, message['message_id'], inline_keyboard)
 
