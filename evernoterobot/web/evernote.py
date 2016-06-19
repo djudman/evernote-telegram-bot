c49b02c02871cbd6a7f71a44ef1c8317b3ebd471
Dmitry Dorofeev <dorofeev@road404.ru>
2016-06-19 17:02:15 +0300

    piece of refactor

--- evernoterobot/web/evernote.py
+++ evernoterobot/web/evernote.py
@@ -4,34 +4,35 @@ from aiohttp import web
 
 async def oauth_callback(request):
     logger = request.app.logger
-    robot = request.app.bot
+    bot = request.app.bot
     params = parse_qs(request.query_string)
     callback_key = params.get('key', [''])[0]
 
-    session = await robot.get_start_session(callback_key)
+    session = await bot.get_start_session(callback_key)
     if not session:
+        logger.error("Session for callback key = %s not found" % callback_key)
         return web.HTTPForbidden()
 
     if params.get('oauth_verifier'):
         oauth_verifier = params['oauth_verifier'][0]
         # TODO: async
-        access_token = robot.evernote.get_access_token(
+        access_token = bot.evernote.get_access_token(
             session.oauth_token,
             session.oauth_token_secret,
             oauth_verifier)
 
         logger.info('evernote access_token = %s' % access_token)
-        await robot.register_user(session, access_token)
+        await bot.register_user(session, access_token)
 
-        notebook = robot.evernote.getDefaultNotebook(access_token)
+        notebook = bot.evernote.getDefaultNotebook(access_token)
         text = "Evernote account is connected.\n\
 Now you can just send message and note be created.\n\
 Current notebook: %s" % notebook.name
-        await robot.send_message(session.user_id, text)
+        await bot.send_message(session.user_id, text)
     else:
         # User decline access
         logger.info('User declined access. No access token =(')
         text = "We are sorry, but you declined authorization 😢"
-        await robot.send_message(session.user_id, text)
+        await bot.send_message(session.user_id, text)
 
-    return web.HTTPFound(robot.bot_url)
+    return web.HTTPFound(bot.url)
