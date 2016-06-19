5bb163833176f8f0742bbb02b03f0ea43182d6c8
Dmitry Dorofeev <djudman@gmail.com>
2016-06-20 00:00:29 +0300

    cleanup

--- evernoterobot/bot/test_evernoterobot.py
+++ evernoterobot/bot/test_evernoterobot.py
@@ -3,7 +3,8 @@ from bot import get_commands
 
 def test_get_commands():
     commands = get_commands()
-    assert len(commands) == 2
+    assert len(commands) == 3
     names = [cmd.name for cmd in commands]
     assert 'help' in names
     assert 'start' in names
+    assert 'notebook' in names
--- evernoterobot/telegram/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-
-    def __init__(self, data):
-        self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data.get('last_name')
-        self.username = data.get('username')
