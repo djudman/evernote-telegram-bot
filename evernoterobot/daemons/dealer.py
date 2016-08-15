885ae6a2d05becc46db66e861367aaf9ba18e6be
Dmitry Dorofeev <dorofeev@road404.ru>
2016-08-15 17:16:46 +0300

    argh!

--- evernoterobot/daemons/dealer.py
+++ evernoterobot/daemons/dealer.py
@@ -44,7 +44,7 @@ class EvernoteDealer:
                 update = await TelegramUpdate.find_and_modify(
                     query={'_id': entry._id, 'in_process': {'$exists': False}},
                     update={'$set': {'in_process': True}})
-                fetched_updates.append(update)
+                fetched_updates.append(TelegramUpdate(**update))
             self.logger.debug('Fetched {0} updates'.format(len(fetched_updates)))
 
             for update in fetched_updates:
