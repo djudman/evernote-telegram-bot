774cbccb913bc3a07170b35455fb324a9bc7d731
Dmitry Dorofeev <dorofeev@road404.ru>
2016-09-01 12:08:51 +0300

    fix

--- evernoterobot/bot/commands/start.py
+++ evernoterobot/bot/commands/start.py
@@ -37,7 +37,7 @@ Please tap on button below to link your Evernote account with bot.'''
         except ModelNotFound:
             User.create(id=user_id,
                         telegram_chat_id=chat_id,
-                        mode='one_note',
+                        mode='multiple_notes',
                         username=message.user.username,
                         first_name=message.user.first_name,
                         last_name=message.user.last_name)
