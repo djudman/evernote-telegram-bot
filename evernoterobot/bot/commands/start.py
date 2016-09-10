55e0c0423cb80e0b745f6d1a4d8249d9eaf2792e
Dmitry Dorofeev <djudman@gmail.com>
2016-09-10 10:03:11 +0300

    start command improvements

--- evernoterobot/bot/commands/start.py
+++ evernoterobot/bot/commands/start.py
@@ -1,5 +1,7 @@
 import json
 
+import asyncio
+
 from bot.model import StartSession, User, ModelNotFound
 from ext.telegram.bot import TelegramBotCommand
 from ext.telegram.models import Message
@@ -19,7 +21,7 @@ Please tap on button below to link your Evernote account with bot.'''
         }
         inline_keyboard = {'inline_keyboard': [[signin_button]]}
         chat_id = message.chat.id
-        msg = await self.bot.api.sendMessage(chat_id, welcome_text, json.dumps(inline_keyboard))
+        welcome_message_future = asyncio.ensure_future(self.bot.api.sendMessage(chat_id, welcome_text, json.dumps(inline_keyboard)))
         # TODO: async
         user_id = message.user.id
         oauth_data = self.bot.evernote.get_oauth_data(user_id)
@@ -39,4 +41,5 @@ Please tap on button below to link your Evernote account with bot.'''
 
         signin_button['text'] = 'Sign in to Evernote'
         signin_button['url'] = oauth_data["oauth_url"]
-        await self.bot.api.editMessageReplyMarkup(chat_id, msg['message_id'], json.dumps(inline_keyboard))
+        msg = welcome_message_future.result()
+        asyncio.ensure_future(self.bot.api.editMessageReplyMarkup(chat_id, msg['message_id'], json.dumps(inline_keyboard)))
