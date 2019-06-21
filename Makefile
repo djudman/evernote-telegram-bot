4c4e564b50746c7bd96d12a704ff19e65ed70ab5
Dmitrii Dorofeev <djudman@gmail.com>
2019-06-21 10:31:15 +0300

    on the road to working bot

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 VERSION := $(shell cat ./VERSION)
 
 httpd:
-	@gunicorn --error-logfile ./logs/errors.log evernotebot.wsgi:app
+	gunicorn --error-logfile ./gunicorn-errors.log evernotebot.wsgi:app
--- evernotebot/wsgi.py
+++ evernotebot/wsgi.py
@@ -1,20 +1,18 @@
 import traceback
-from time import time
-from os.path import dirname, realpath, join
+from os.path import dirname, realpath
 from urllib.parse import urlparse
 
 from evernotebot.config import load_config
 from evernotebot.bot.core import EvernoteBot
-from evernotebot.util.telegram.models import TelegramUpdate
 from uhttp import WsgiApplication
 from uhttp.core import HTTPFound
+from utelegram.models import Update
 
 
 def telegram_hook(request):
-    bot = request.app.bot
     data = request.json()
-    telegram_update = TelegramUpdate(data)
-    bot.handle_telegram_update(telegram_update)
+    telegram_update = Update(**data)
+    request.app.bot.process_update(telegram_update)
 
 
 def evernote_oauth(request):
@@ -26,17 +24,25 @@ def evernote_oauth(request):
     return HTTPFound(bot.url)
 
 
-config = load_config()
-webhook_url = config['webhook_url']
-src_root = realpath(dirname(__file__))
-app = WsgiApplication(src_root, config=config, urls=(
-    ('POST', urlparse(webhook_url).path, telegram_hook),
-    ('GET', r'^/evernote/oauth$', evernote_oauth),
-))
-app.bot = EvernoteBot(config)
+def create_app():
+    config = load_config()
+    webhook_url = config['webhook_url']
+    webhook_path = urlparse(webhook_url).path
+    src_root = realpath(dirname(__file__))
+    app = WsgiApplication(src_root, config=config, urls=(
+        ('POST', webhook_path, telegram_hook),
+        ('GET', r'^/evernote/oauth$', evernote_oauth),
+    ))
+    app.bot = EvernoteBot(config)
+    return app
+
+
+app = create_app()
+webhook_url = app.config['webhook_url']
 try:
     app.bot.api.setWebhook(webhook_url)
 except Exception as e:
     str_traceback = ''.join(traceback.format_tb(e.__traceback__))
-    message = 'Can\'t set up webhook url `{url}`. Exception:\n{exception}'.format(url=webhook_url, exception=str_traceback)
+    message = f"Can't set up webhook url `{webhook_url}`. "\
+              f"Exception:\n{str_traceback}"
     app.log(level='error', message=message)
