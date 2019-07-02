8ade083d09b66a950de352931128492141a20ea5
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-07-02 14:27:16 +0300

    testing of core

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest import mock
 
+from utelegram.models import Message
 from evernotebot.bot.core import EvernoteBot, EvernoteBotException
 from evernotebot.bot.models import BotUser
 
@@ -70,3 +71,61 @@ class TestCore(unittest.TestCase):
         self.assertEqual(bot_user.evernote.notebook.guid, "xxx")
         bot.switch_notebook(bot_user, "xxx")
         self.assertEqual(bot_user.evernote.notebook.guid, "xxx")
+
+    def test_switch_mode(self):
+        bot_user = BotUser(**self.default_user_data)
+        bot = EvernoteBot(bot_config)
+        with self.assertRaises(EvernoteBotException) as ctx:
+            bot.switch_mode(bot_user, "invalid")
+        self.assertEqual(str(ctx.exception), "Unknown mode 'invalid'")
+
+        bot.api = mock.Mock()
+        bot.api.sendMessage = mock.Mock()
+        bot.switch_mode(bot_user, "multiple_notes")
+        bot.api.sendMessage.assert_called_once_with(1,
+            "The bot already in 'multiple_notes' mode.",
+            "{\"hide_keyboard\": true}")
+
+        bot.switch_mode_one_note = mock.Mock()
+        bot.switch_mode(bot_user, "one_note")
+        bot.switch_mode_one_note.assert_called_once()
+
+        bot_user.bot_mode = "one_note"
+        bot_user.evernote.shared_note_id = 123
+        bot.api.sendMessage = mock.Mock()
+        bot.switch_mode(bot_user, "multiple_notes")
+        bot.api.sendMessage.assert_called_once_with(1,
+            "The bot has switched to 'multiple_notes' mode.",
+            "{\"hide_keyboard\": true}")
+        self.assertIsNone(bot_user.evernote.shared_note_id)
+        self.assertEqual(bot_user.bot_mode, "multiple_notes")
+
+    def test_location(self):
+        bot = EvernoteBot(bot_config)
+        bot.storage = mock.Mock()
+        bot.storage.get = mock.Mock(return_value=self.default_user_data)
+        bot.save_note = mock.Mock()
+        message = Message(
+            message_id=1,
+            date=1,
+            from_user={"id": 2, "is_bot": False, "first_name": "John"},
+            location = {"longitude": 1.2, "latitude": 3.4},
+            venue = {
+                "location": {"longitude": 1.2, "latitude": 3.4},
+                "title": "Kremlin",
+                "address": "Red Square, 1",
+                "foursquare_id": "123",
+            }
+        )
+        bot.on_location(message)
+        bot.save_note.assert_called_once()
+        user = bot.save_note.call_args[0][0]
+        self.assertIsInstance(user, BotUser)
+        title = bot.save_note.call_args[1]["title"]
+        html = bot.save_note.call_args[1]["html"]
+        self.assertEqual(title, "Kremlin")
+        self.assertEqual(html, "Kremlin<br />Red Square, 1<br />"
+        "<a href='https://maps.google.com/maps?q=3.4,1.2'>"
+            "https://maps.google.com/maps?q=3.4,1.2"
+        "</a><br />"
+        "<a href='https://foursquare.com/v/123'>https://foursquare.com/v/123</a>")
