d11c644ac5323eec28ae7741dbe9d22629690bd4
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-07-16 20:43:30 +0300

    return jwt token to authenticated client

--- evernotebot/web/admin/api.py
+++ evernotebot/web/admin/api.py
@@ -1,10 +1,24 @@
 import hashlib
 import json
 import math
+import random
+import string
 from time import time
 
 from uhttp.core import HTTPFound, Request, Response
 from uhttp.shortcuts import auth_required
+
+from evernotebot.web.admin.jwt import create_token
+
+
+def api_response(status=True, data=None, error=None, headers=None):
+    response = { "status": status }
+    if data is not None:
+        response["data"] = data
+    if error is not None:
+        response["error"] = error
+    body = json.dumps(response)
+    return Response(body, headers)
 
 
 def api_login(request: Request):
@@ -12,18 +26,19 @@ def api_login(request: Request):
     password = request.GET["password"]
     password_hash = hashlib.sha256(password.encode()).hexdigest()
     config = request.app.config
-    admins = request.app.config["uhttp"]["admins"]
-    secret = config["secret"]
+    admins = config["uhttp"]["admins"]
     for admin_username, admin_password_hash in admins:
         if admin_username == username and admin_password_hash == password_hash:
-            key = f"{time()}{secret}{password_hash}"
-            auth_token = hashlib.sha256(key.encode()).hexdigest()
+            alphabet = "".join([string.ascii_letters + string.digits])
+            salt = "".join([random.choice(alphabet) for _ in range(16)])
+            sid = f"{salt}{username}{password_hash}"
+            sid = "{0}.{1}".format(salt, hashlib.sha256(sid.encode()).hexdigest())
+            jwt_token = create_token({"sid": sid}, config["secret"])
             headers = [
-                ("Location", "/"),
-                ("Set-Cookie", f"auth_token={auth_token}; Secure; SameSite=Strict")
+                ("Set-Cookie", f"token={jwt_token}; Secure; SameSite=Strict"),
             ]
-            return Response(None, 302, headers=headers)
-    return Response(None, 401)
+            return api_response(data={"token": jwt_token}, headers=headers)
+    return api_response(False, error="Access denied")
 
 
 @auth_required
@@ -40,25 +55,26 @@ def api_get_logs(request: Request):
     page_size = int(request.GET.get("page_size", 10))
     num_pages = math.ceil(total_cnt / page_size)
     start = page_size * page
-    return {
+    data = {
         "total": total_cnt,
         "num_pages": num_pages,
         "data": data[start:(start + page_size)],
     }
+    return api_response(data=data)
 
 
 @auth_required
 def api_list_failed_updates(request: Request):
     bot = request.app.bot
     bot.failed_updates.get_all()
-    raise Exception("Not implemented")
+    return api_response(error="Not implemented")
 
 
 @auth_required
 def api_retry_failed_update(request: Request):
-    raise Exception("Not implemented")
+    return api_response(error="Not implemented")
 
 
 @auth_required
 def api_send_broadcast_message(request: Request):  # to all users
-    raise Exception("Not implemented")
+    return api_response(error="Not implemented")
