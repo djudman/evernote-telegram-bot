b478f0f1c1632d07e71048e6591b6a497851d44b
Dmitrii Dorofeev <dorofeev@game-insight.com>
2019-09-03 17:33:50 +0300

    config loading fixed

--- evernotebot/config.py
+++ evernotebot/config.py
@@ -4,21 +4,15 @@ import re
 from logging import Formatter
 
 
-_config_cache = None
-
-
 def load_config():
-    global _config_cache
-    if _config_cache is not None:
-        return _config_cache
-    defaults = {
-        'MONGO_HOST': '127.0.0.1',
-        'EVERNOTEBOT_DEBUG': False,
-    }
     filename = 'evernotebot.config.json'
     with open(filename, 'r') as f:
         config_str_data = f.read()
     matches = re.findall(r'\$\{([0-9a-zA-Z_]+)\}', config_str_data)
+    defaults = {
+        'MONGO_HOST': '127.0.0.1',
+        'EVERNOTEBOT_DEBUG': False,
+    }
     for name in matches:
         value = os.getenv(name, defaults.get(name))
         if value is None:
@@ -26,9 +20,7 @@ def load_config():
         if not isinstance(value, str):
             value = str(value).lower()
         config_str_data = config_str_data.replace(f'${{{name}}}', value)
-    config = json.loads(config_str_data)
-    _config_cache = config
-    return config
+    return json.loads(config_str_data)
 
 
 class JsonFormatter(Formatter):
