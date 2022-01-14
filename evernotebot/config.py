4e5547c5f66460885251628a8a19cb2bfa2f96d7
Dorofeev Dmitrii <djudman@gmail.com>
2022-01-14 12:15:29 +0300

    refactor config and config loading

--- evernotebot/config.py
+++ evernotebot/config.py
@@ -1,43 +1,32 @@
 import os
 from pathlib import Path
 
+import re
 import yaml
 
 from evernotebot.util.logs import init_logging
 
 
+env_matcher = re.compile(r'\$\{([^{}]+)\}(:(.+))?')
+
+def env_constructor(loader, node):
+    value = node.value
+    match = env_matcher.match(value)
+    var_name, _, default = match.groups()
+    env_value = os.environ.get(var_name, default)
+    if env_value is not None:
+        return yaml.safe_load(env_value)
+
+class EnvVarLoader(yaml.SafeLoader):
+    pass
+
+EnvVarLoader.add_implicit_resolver('!env', env_matcher, None)
+EnvVarLoader.add_constructor('!env', env_constructor)
+
+
 def load_config():
     filename = Path(__file__).parent.parent.joinpath('config.yaml').absolute()
     with open(filename, 'r') as f:
-        config = yaml.safe_load(f)
-    read_env(config)
-    init_logging(config['debug'])
+        config = yaml.load(f, Loader=EnvVarLoader)
+    init_logging(debug=config['debug'])
     return config
-
-
-def getenv(name: str, default=None, required=False):
-    value = os.getenv(name, default)
-    if required and value is None:
-        raise Exception(f'Environment variable `{name}` is not set')
-    return value
-
-
-def read_env(config):
-    config['debug'] = getenv('EVERNOTEBOT_DEBUG', False)
-    config['host'] = getenv('EVERNOTEBOT_HOSTNAME', 'evernotebot.djud.me')
-    config['telegram'] = {
-        'bot_name': getenv('TELEGRAM_BOT_NAME', 'evernotebot'),
-        'token': getenv('TELEGRAM_API_TOKEN', required=True),
-    }
-    config['evernote'] = {
-        'access': {
-            'basic': {
-                'key': getenv('EVERNOTE_BASIC_ACCESS_KEY', required=True),
-                'secret': getenv('EVERNOTE_BASIC_ACCESS_SECRET', required=True),
-            },
-            'full': {
-                'key': getenv('EVERNOTE_FULL_ACCESS_KEY', required=True),
-                'secret': getenv('EVERNOTE_FULL_ACCESS_SECRET', required=True),
-            },
-        },
-    }
