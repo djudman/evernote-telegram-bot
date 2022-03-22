20df07db6b6f906e41882e78ea0927f7d3ecd2f6
Dmitrii Dorofeev <djudman@gmail.com>
2022-03-22 22:26:25 +0300

    delete useless function run()

--- httpd.py
+++ httpd.py
@@ -5,25 +5,19 @@ from evernotebot.app import EvernoteBotApplication
 
 host = '127.0.0.1'
 port = 8080
-envs = (
+env_vars = (
     'TELEGRAM_API_TOKEN',
     'EVERNOTE_BASIC_ACCESS_KEY',
     'EVERNOTE_BASIC_ACCESS_SECRET',
     'EVERNOTE_FULL_ACCESS_KEY',
     'EVERNOTE_FULL_ACCESS_SECRET',
 )
-
-
-def run():
-    for name in envs:
-        os.environ[name] = 'debug'
-    app = EvernoteBotApplication()
-    try:
-        with make_server(host, port, app) as httpd:
-            print(f'Starting `evernotebot` at http://{host}:{port}...')
-            httpd.serve_forever()
-    except KeyboardInterrupt:
-        app.shutdown()
-
-
-run()
+app = EvernoteBotApplication()
+for name in env_vars:
+    os.environ[name] = 'debug'
+try:
+    with make_server(host, port, app) as httpd:
+        print(f'Starting `evernotebot` at http://{host}:{port}...')
+        httpd.serve_forever()
+except KeyboardInterrupt:
+    app.shutdown()
