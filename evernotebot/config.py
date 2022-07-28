47ea661db0d452ccebd98e68d40d65d034da08a9
Dmitrii Dorofeev <djudman@gmail.com>
2022-07-28 12:01:25 +0300

    get default values right

--- evernotebot/config.py
+++ evernotebot/config.py
@@ -18,13 +18,12 @@ def make_dirs(config):
         os.makedirs(path, exist_ok=True)
 
 
-
 def load_config():
-    bot_name = os.getenv('TELEGRAM_BOT_NAME', 'evernoterobot')
-    host = os.getenv('EVERNOTEBOT_HOSTNAME', '127.0.0.1')
-    port = os.getenv('EVERNOTEBOT_EXPOSE_PORT', 8000)
-    bot_api_token = os.getenv('TELEGRAM_API_TOKEN', 'bot_api_token')
-    is_debug = os.getenv('EVERNOTEBOT_DEBUG')
+    bot_name = os.getenv('TELEGRAM_BOT_NAME') or 'evernoterobot'
+    host = os.getenv('EVERNOTEBOT_HOSTNAME') or '127.0.0.1'
+    port = os.getenv('EVERNOTEBOT_EXPOSE_PORT') or 8000
+    bot_api_token = os.getenv('TELEGRAM_API_TOKEN') or 'bot_api_token'
+    is_debug = bool(os.getenv('EVERNOTEBOT_DEBUG'))
     oauth_url = (host == '127.0.0.1') and f'{host}:{port}/evernote/oauth' or f'{host}/evernote/oauth'
     default_oauth_url = is_debug and f'http://{oauth_url}' or f'https://{oauth_url}'
     default_bot_api_url = is_debug and 'http://127.0.0.1:11000/' or 'https://api.telegram.org/'
@@ -35,14 +34,14 @@ def load_config():
         'default_mode': 'multiple_notes',
         'host': host,
         'port': port,
-        'oauth_callback_url': os.getenv('OAUTH_CALLBACK_URL', default_oauth_url),
-        'webhook_url': os.getenv('WEBHOOK_URL', default_webhook_url),
+        'oauth_callback_url': os.getenv('OAUTH_CALLBACK_URL') or default_oauth_url,
+        'webhook_url': os.getenv('WEBHOOK_URL') or default_webhook_url,
         'logs_root': root_dir('logs/'),
-        'tmp_root': os.getenv('TMP_ROOT', root_dir('tmp/')),
+        'tmp_root': os.getenv('TMP_ROOT') or root_dir('tmp/'),
         'telegram': {
             'bot_name': bot_name,
             'token': bot_api_token,
-            'api_url': os.getenv('BOT_API_URL', default_bot_api_url)
+            'api_url': os.getenv('BOT_API_URL') or default_bot_api_url
         },
         'evernote': {
             'access': {
