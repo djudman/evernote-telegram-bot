92701f54c3cd02829c629b5f11fbf7845f2a04e6
Dmitrii Dorofeev <djudman@gmail.com>
2022-08-16 15:04:09 +0300

    types added

--- evernotebot/bot/mixins/bot_api.py
+++ evernotebot/bot/mixins/bot_api.py
@@ -5,6 +5,6 @@ from evernotebot.bot.mixins.base import BaseMixin
 class BotApiMixin(BaseMixin):
     def __init__(self, config: dict):
         super(BotApiMixin, self).__init__(config)
-        token = config['telegram']['token']
-        bot_api_url = config['telegram']['api_url']
-        self.api = BotApi(token, bot_api_url)
+        token: str = config['telegram']['token']
+        bot_api_url: str = config['telegram']['api_url']
+        self.api: BotApi = BotApi(token, bot_api_url)
